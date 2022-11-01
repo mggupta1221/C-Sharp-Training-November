e7a9de369bf26a2dfdf787bffdd65a33689ae616
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-11-01 12:11:44 +0530

    Dictionary part

---
+++ 01-11-22/DictionaryDemo/DemoDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryDemo
+{
+    public class DemoDictionary
+    {
+
+         private Dictionary<string, object> employees = new Dictionary<string, object>();
+
+        public void AddEmployee1()
+        {
+            employees.Add("Eid", 100);
+            employees.Add("EName", "Scott");
+            employees.Add("Salary", 1000000);
+            employees.Add("Dname","IT");
+            employees.Add("Email","Scotttwatt@gmail.com");
+            employees.Add("Location","Pune");
+            employees.Add("Designation", "Analyst");
+            employees.Add("Mgrid", 200);
+        }
+        public void RetreiveEmployee()
+        {
+            foreach(var employeeKey in employees.Keys)
+            {
+                Console.WriteLine($"{employeeKey}: {employees[employeeKey]}");
+            }
+        }
+          
+    }
+}
---
+++ 01-11-22/DictionaryDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DictionaryDemo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DemoDictionary obj = new DemoDictionary();
+            obj.AddEmployee1();
+            obj.RetreiveEmployee();
+
+        }
+    }
+}
