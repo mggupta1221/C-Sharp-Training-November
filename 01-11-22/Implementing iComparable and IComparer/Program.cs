b0899ff6f52f0c5166a8e355bd4fc62d19eac758
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-11-01 13:00:46 +0530

    implementing icomparable and icomparer

---
+++ 01-11-22/Implementing iComparable and IComparer/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Implementing_iComparable_and_IComparer
+{
+    class Program
+    {
+        public static void Main()
+        {
+            StudentRepository studentRepositoryobj = new StudentRepository();
+            Console.WriteLine("Enter \n1.sorting based on id\n2.Sorting based on grade");
+            int choice=int.Parse(Console.ReadLine());
+            studentRepositoryobj.AddStudents(choice);
+            studentRepositoryobj.ShowStudents();
+        }
+    }
+}
---
+++ 01-11-22/Implementing iComparable and IComparer/StudentRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Linq;
+
+namespace Implementing_iComparable_and_IComparer
+{
+    class StudentRepository
+    {
+        List<Student> students = new List<Student>();
+        Student2 student2obj=new Student2();    
+        public void AddStudents(int choice)
+        {
+            students.Add(new Student(){ Id = 101,Name = "Marsh",Grade=7.8});
+            students.Add(new Student() { Id = 1, Name = "Lenin", Grade = 8.9 });
+            students.Add(new Student() { Id = 55, Name = "Scott", Grade = 9.4});
+            students.Add(new Student() { Id = 99, Name = "Micheal", Grade = 10 });
+            if(choice == 1)
+                students.Sort();
+            else
+                students.Sort(student2obj); //overloading method
+        }
+
+        public void ShowStudents()
+        {
+            foreach (Student student in students)
+                Console.WriteLine($"{student.Id} : {student.Name} : {student.Grade} ");
+        }
+    }
+}
