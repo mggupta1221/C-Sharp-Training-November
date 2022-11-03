4380d4fbe801da2695b4ac4c52ba853abab1d33b
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-11-03 10:32:20 +0530

    LinkedList Implementation

---
+++ 03-11-22/LinkedList/SimulateJourney/Bus.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulateJourney
+{
+    public class Bus
+    {
+        public const int CAPACITY = 5;
+        public int Space { get { return CAPACITY - _passengers.Count; } }
+
+        private LinkedList <Passenger> _passengers=new LinkedList<Passenger>();
+
+        public bool Load(Passenger passenger)
+        {
+            if (Space < 1)
+                return false;
+
+           _passengers.AddLast(passenger);
+            Console.WriteLine($"{passenger} got on Bus");
+            return true;
+        }
+        public void ArriveAt(string place)
+        {
+
+            Console.WriteLine($"\n Bus Arriving At terminus");
+
+            if (_passengers.Count == 0)
+                return;
+      
+                LinkedListNode<Passenger> currentNode = _passengers.First;
+                while (_passengers != null)
+                {
+                    LinkedListNode<Passenger> nextNode = currentNode.Next;
+                    if (currentNode.Value.Destination == place)
+                    {
+                        Console.WriteLine($"{currentNode.Value.Name} is getting off at {place}");
+                        _passengers.Remove(currentNode);
+
+                    }
+                    currentNode = nextNode;
+                }
+                Console.WriteLine($"There are {_passengers.Count} persons are left in bus"); 
+        }
+
+    }
+}
---
+++ 03-11-22/LinkedList/SimulateJourney/PassengerGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulateJourney
+{
+    public class PassengerGenerator
+    {
+        private static int _count;
+
+        public static Passenger CreatePassenger()
+        {
+        
+            Random randomNumberGenerator=new Random();
+            int randomNumber= randomNumberGenerator.Next(1,3);
+            string Destination = "";
+
+            if (randomNumber % 2 == 0)
+                Destination = "Lancaster";
+            else
+                Destination = "Pune";
+
+            return new Passenger($"Person {++ _count}", Destination);
+        }
+    }
+}
