ca93e3e7756e1e6138d504b800bcb34e5c464fc5
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-11-04 17:47:52 +0530

    LINQ filtering where and searching

---
+++ 04-11-22 LINQ/Filtering using where and extracting or searching/Module04/LINQSamples/RepositoryClasses/ProductHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQSamples.RepositoryClasses
+{
+    public static class ProductHelper
+    {
+        public static IEnumerable<Product> ByColor(this IEnumerable<Product> query,string color)
+        {
+            return query.Where(prod => prod.Color == color);
+        }
+    }
+}
