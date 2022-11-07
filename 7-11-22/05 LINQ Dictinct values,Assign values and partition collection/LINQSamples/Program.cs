12a2c22ba614480a13a13157f3eb79f77f8d903d
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-11-07 11:09:00 +0530

    LINQ DISTINCT VALUES<ASSIGN VALUES USING FOREACH AND PARTITION  COLLECTION

---
+++ 7-11-22/05 LINQ Dictinct values,Assign values and partition collection/LINQSamples/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LINQSamples
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Instantiate the ViewModel
+      SamplesViewModel vm = new SamplesViewModel
+      {
+        // Use Query or Method Syntax?
+        UseQuerySyntax = true
+      };
+
+            // Call a sample method
+            //vm.ForEach();
+            //vm.ForEachCallingMethod();
+            //vm.Take();
+            //vm.TakeWhile(); 
+            //vm.Skip();
+            //vm.SkipWhile();
+            vm.Distinct();
+
+
+      // Display Product Collection
+      foreach (var item in vm.Products) {
+        Console.Write(item.ToString());
+      }
+
+      // Display Result Text
+      Console.WriteLine(vm.ResultText);
+
+
+    }
+  }
+}
