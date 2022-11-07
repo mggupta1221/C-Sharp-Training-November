b60900b36c3acc0e792dc3b78158e80b45aed691
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-11-07 12:01:49 +0530

    06 All any Contains EquaityComparer

---
+++ 7-11-22/06 All any Contains EquaityComparer/LINQSamples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LINQSamples
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Instantiate the Samples ViewModel
+      SamplesViewModel vm = new SamplesViewModel
+      {
+        // Use Query or Method Syntax?
+        UseQuerySyntax = true
+      };
+
+            // Call a sample method
+            // vm.Any();
+            //vm.All();
+            //vm.LINQContains();
+            vm.LINQContainsUsingComparer();
+
+      // Display Product Collection
+      foreach (var item in vm.Products) {
+        Console.Write(item.ToString());
+      }
+
+      // Display Result Text
+      Console.WriteLine(vm.ResultText);
+    }
+  }
+}
